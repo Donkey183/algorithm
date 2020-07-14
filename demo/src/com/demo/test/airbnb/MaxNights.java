a06cfa4b0fb2375c03aa2bb086d1ccb328118277
weijiangmin <weijiangmin@didichuxing.com>
2020-07-14 13:26:33 +0800

    update

--- demo/src/com/demo/test/airbnb/MaxNights.java
+++ demo/src/com/demo/test/airbnb/MaxNights.java
@@ -1,7 +1,7 @@
 package com.demo.test.airbnb;
 
 public class MaxNights {
-  
+
   public static int findMaxDays(int[] days, int i) {
     if (i == days.length - 1) {
       return days[days.length - 1];
@@ -11,26 +11,22 @@ public class MaxNights {
     }
     return Math.max(days[i] + findMaxDays(days, i + 2), days[i] + findMaxDays(days, i + 3));
   }
-  
+
   /**
-   * Provide a set of positive integers (an array of integers ).
-   * Each integer represent number of nights user request on Airbnb.com.
-   * If you are a host, you need to design and implement an algorithm to
-   * find out the maximum number a nights you can accommodate.
-   * The constrain is that you have to reserve at least one day between each request,
-   * so that you have time to clean the room.
-   * Example: 1) Input: [1, 2, 3] ===&gt; output: 4, because you will pick 1 and 3
-   * 2)input: [5, 1, 2, 6] ===&gt; output: 11, because you will pick 5 and 6
-   * 3)input: [5, 1, 2, 6, 20, 2] ===&gt; output: 27, because you will pick 5, 2, 20
-   *
-   * @param args
+   * Provide a set of positive integers (an array of integers ). Each integer represent number of
+   * nights user request on Airbnb.com. If you are a host, you need to design and implement an
+   * algorithm to find out the maximum number a nights you can accommodate. The constrain is that
+   * you have to reserve at least one day between each request, so that you have time to clean the
+   * room. Example: 1) Input: [1, 2, 3] ===&gt; output: 4, because you will pick 1 and 3 2)input:
+   * [5, 1, 2, 6] ===&gt; output: 11, because you will pick 5 and 6 3)input: [5, 1, 2, 6, 20, 2]
+   * ===&gt; output: 27, because you will pick 5, 2, 20
    */
   public static void main(String[] args) {
     int[] one = {1, 2, 3};
     int[] two = {5, 1, 2, 6};
     int[] three = {5, 1, 2, 6, 20, 2};
-    int result = findMaxDays(one, 0);
-    System.out.println(result);
+    System.out.println(findMaxDays(one, 0));
+    System.out.println(findMaxDays(two, 0));
+    System.out.println(findMaxDays(three, 0));
   }
-  
 }
