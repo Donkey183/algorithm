65ae990f4c92be71ae60ba8483c870c0c3e70e61
donkey183@126.com <donkey183@126.com>
2023-04-06 23:02:19 +0800

    update

--- demo/src/com/demo/test/I/Test.java
+++ demo/src/com/demo/test/I/Test.java
@@ -1,6 +1,24 @@
 package com.demo.test.I;
 
-import java.util.Arrays;
+import com.demo.test.链表.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
+
+    public static void main(String[] args) {
+//        findSum(5, 9);
+    }
+
+    public static ListNode reverse(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            newHead = head.next;
+
+            head = next;
+        }
+        return newHead;
+    }
 }
